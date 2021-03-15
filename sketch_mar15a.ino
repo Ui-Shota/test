c73501ee8c2fadcd693bf9b574ca156b953eafb6
Ui Shota <e5a2001567@edu.teu.ac.jp>
2021-03-15 17:48:43 +0900

    test

---
+++ sketch_mar15a.ino
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+void setup(){
+    pinMode(PIN_LED2, OUTPUT);
+}
+ 
+void loop(){
+    digitalWrite(PIN_LED2, HIGH);
+    delay(100);
+    digitalWrite(PIN_LED2, LOW);
+    delay(100);
+}
